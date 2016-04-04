e3a9bdf8fb32d3c44463fcb68a8b267e94f14275
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2016-04-04 17:41:50 -0400

    Re-added comment from original source.

--- random.go
+++ random.go
@@ -19,6 +19,7 @@ var src = rand.NewSource(time.Now().UnixNano())
 
 func RandomString(n int) string {
 	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
 	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
 			cache, remain = src.Int63(), letterIdxMax
