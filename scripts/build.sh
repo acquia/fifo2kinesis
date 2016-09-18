5dc4d1ec05df8c67750ae86fc455ad96ee7d31f9
Chris Pliakas <cpliakas@gmail.com>
2016-09-17 22:33:54 -0400

    Added build script, updated gitignore [skip ci]

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd $(dirname $0)/..
+GOPATH=$PWD
+
+version=$(git describe --exact-match --tags HEAD 2> /dev/null)
+if [ -z "$version" ]; then
+  version=$(git rev-parse --short HEAD)
+fi
+
+GOOS=linux GOARCH=amd64 gb build
+GOOS=darwin GOARCH=amd64 gb build
+
+mv fifo2kinesis-linux-amd64 fifo2kinesis-$version-linux-amd64
+mv fifo2kinesis-darwin-amd64 fifo2kinesis-$version-darwin-amd64
+
+
